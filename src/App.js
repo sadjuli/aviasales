b1b6ffe72c0e6867fdfe69938cf3132976806838
sadjuli <Yulya-sadkeeva@yandex.ru>
2024-09-18 03:01:45 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCompanies } from './store/companiesSlice'
+import { fetchFlights } from './store/flightsSlice'
+import Header from './components/Header'
+import Filters from './components/Filters'
+import Sort from './components/Sort'
+import FlightList from './components/FlightList'
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch()
+  const companies = useSelector((state) => { 
+    const allCompanies = state.companies.ids.map(id => state.companies.entities[id]);
+    return allCompanies;
+  });
+  const loading = useSelector((state) => state.companies.loading);
+  const flights = useSelector((state) => { 
+    const allFlights = state.flights.ids.map(id => state.flights.entities[id]);
+    return allFlights;
+  })
+  
+  const [mode, setMode] = useState('cheap')
+  const [connectionsFilter, setConnectionsFilter] = useState([])
+  const [companiesFilter, setCompaniesFilter] = useState([])
+  const [showAmount, setShowAmount] = useState(3)
+
+  useEffect(() => {
+    dispatch(fetchCompanies())
+    dispatch(fetchFlights())
+  }, [dispatch])
+
+  const handleLoadMore = () => {
+    setShowAmount((prev) => prev + 3)
+  }
+
+  const handleMode = (value) => {
+    setMode(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className="main">
+        <div className="sidebar">
+          <Filters loading={loading} companies={companies} setCompaniesFilter={setCompaniesFilter} setConnectionsFilter={setConnectionsFilter} />
+        </div>
+        <div className='content'>
+          <Sort mode={mode} handleMode={handleMode} />
+          <FlightList
+            companies={companies}
+            flights={flights}
+            mode={mode}
+            connectionsFilter={connectionsFilter}
+            companiesFilter={companiesFilter}
+            showAmount={showAmount}
+            onLoadMore={handleLoadMore}
+          />
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
